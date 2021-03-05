a5864d5ebb18ed022c0f0d21b04a9df9b925fbe2
Joe Cartwright <joecartwright696@gmail.com>
2021-03-05 15:06:59 +0000

    Added HelloWorld class

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * HelloWorld.class
+ * Author: Joe Cartwright
+ * Date: 05.03.2021
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
